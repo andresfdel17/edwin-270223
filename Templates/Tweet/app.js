d929650fbe9d355aef47e102ef91fd3d77f88789
andresfdel17 <andresfdel13@hotmail.com>
2023-02-27 20:55:12 -0500

    fix: ajustando css y funcionalidad

---
+++ Templates/Tweet/app.js
@@ -0,0 +1,15 @@
+"use strict";
+const textChars = document.getElementById("textChars");
+const leftChars = document.getElementById("leftChars");
+const TOTALCHARS = 250;
+textChars === null || textChars === void 0 ? void 0 : textChars.addEventListener("keyup", (e) => {
+    var _a;
+    const left = TOTALCHARS - ((_a = textChars === null || textChars === void 0 ? void 0 : textChars.value) === null || _a === void 0 ? void 0 : _a.length);
+    const leftText = `Quedan ${left} caracteres`;
+    if (left <= 50)
+        leftChars.style.color = "red";
+    if (left > 50)
+        leftChars.style.color = "black";
+    leftChars.textContent = leftText;
+});
+//# sourceMappingURL=app.js.map
---
+++ Templates/Tweet/app.ts
@@ -0,0 +1,11 @@
+const textChars: any = document.getElementById("textChars");
+const leftChars: any = document.getElementById("leftChars");
+const TOTALCHARS = 250;
+
+textChars?.addEventListener("keyup", (e: any) => {
+    const left = TOTALCHARS - textChars?.value?.length;
+    const leftText = `Quedan ${left} caracteres`;
+    if(left <= 50) leftChars.style.color = "red";
+    if(left > 50) leftChars.style.color = "black";
+    leftChars.textContent = leftText;
+})
